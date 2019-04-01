0a10cf2f04e8da955ab0c57347a01179575dce7c
Linda Lee <redtaq@hotmail.com>
2019-03-31 19:25:11 -0500

    Updates

---
+++ precache-manifest.b5f897d0b0f72bace426e2b71d3d463c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "afe290d96b0fe27bed9f",
+    "url": "/slidingPuzzle/static/css/main.e015559d.chunk.css"
+  },
+  {
+    "revision": "afe290d96b0fe27bed9f",
+    "url": "/slidingPuzzle/static/js/main.9ebe4d91.chunk.js"
+  },
+  {
+    "revision": "73e510dce653881f79fa",
+    "url": "/slidingPuzzle/static/js/runtime~main.738b7802.js"
+  },
+  {
+    "revision": "3f1e1ca1a787d240ea9a",
+    "url": "/slidingPuzzle/static/js/2.78e4e997.chunk.js"
+  },
+  {
+    "revision": "2c56f86b66fd290fa02c56b526ce2f1d",
+    "url": "/slidingPuzzle/static/media/back.2c56f86b.jpg"
+  },
+  {
+    "revision": "84f891bd242b6f91b318ebbcf6ed4ef5",
+    "url": "/slidingPuzzle/index.html"
+  }
+];
